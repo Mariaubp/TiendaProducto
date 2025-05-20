67d4f09942ba297938ab1dee09aa50246987c168
Maria Constanza Gigli <76410958+Mariaubp@users.noreply.github.com>
2025-05-20 00:06:43 -0300

    Delete TiendaProducto directory

--- TiendaProducto/src/main/java/tienda/tiendaproducto/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package tienda.tiendaproducto.repository;
-
-import tienda.tiendaproducto.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Producto, Long> {
-    // Consultas personalizadas si las necesitas
-}
